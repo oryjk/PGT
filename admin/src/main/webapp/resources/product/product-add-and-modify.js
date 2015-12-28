b3104d7008cd93bbfdf42c1b39aea7d5058e48e1
supersoup <819127251@qq.com>
2015-12-28 19:29:24 +0800

    product-add-and-modify

---
+++ admin/src/main/webapp/resources/product/product-add-and-modify.js
@@ -0,0 +1,14 @@
+/**
+ * Created by supersoup on 15/12/28.
+ */
+$('.pgt-file-btn').change(function() {
+    var that = $(this);
+    var form = that.parent();
+    var p = form.siblings('p');
+    p.html(that.val());
+
+    form.ajaxSubmit(function(param) {
+
+    });
+
+});
