57b74b03f306515099e5c1b847b8cb8d801d545f
Yove <billlllyove@hotmail.com>
2016-01-05 01:47:46 +0800

    2016-01-05 01:47 Logic for login

--- admin/src/main/java/com/pgt/dashboard/DashboardController.java
+++ admin/src/main/java/com/pgt/dashboard/DashboardController.java
@@ -1,5 +1,8 @@
 package com.pgt.dashboard;
 
+import com.pgt.internal.controller.InternalTransactionBaseController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,10 +11,14 @@ import org.springframework.web.servlet.ModelAndView;
  * Created by Yove on 1/4/2016.
  */
 @RestController
-public class DashboardController {
+public class DashboardController extends InternalTransactionBaseController {
 
-	@RequestMapping("/index")
+	private static final Logger LOGGER = LoggerFactory.getLogger(DashboardController.class);
+
+	@RequestMapping("/dashboard")
 	public ModelAndView redirectDashboard() {
 		return new ModelAndView("/dashboard/dashboard");
 	}
+
+
 }
