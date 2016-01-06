786cff34ced6f2ff17b141a07535ed1630061e16
zhangxiaodong <625188964@qq.com>
2016-01-07 03:11:29 +0800

    add banner_image_url sql

--- core/src/main/java/com/pgt/common/controller/BannerController.java
+++ core/src/main/java/com/pgt/common/controller/BannerController.java
@@ -2,55 +2,83 @@ package com.pgt.common.controller;
 
 import java.util.List;
 
-import org.springframework.ui.Model;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.pgt.common.bean.Banner;
 import com.pgt.common.service.BannerService;
 import com.pgt.configuration.URLConfiguration;
 
+
+@RestController
+@RequestMapping("/banner")
 public class BannerController {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(BannerController.class);
+
+	@Autowired
 	private BannerService bannerService;
 
+	@Autowired
 	private URLConfiguration urlConfiguration;
 
 	/********************* 后台 **********************/
 
-	// 1.进入banner列表
+	@RequestMapping(value="/bannerList",method = RequestMethod.GET)
 	public ModelAndView bannerList(ModelAndView modelAndView) {
 
 		List<Banner> banners = bannerService.queryAllBanner();
-
 		modelAndView.addObject("banners", banners);
-
+        modelAndView.setViewName("");
 		return modelAndView;
 	}
 
-	// 2.进入修改banner的页面
-	public String updateBannerUI(Integer bannerId, Model model) {
+	@RequestMapping(value="/addBanner",method = RequestMethod.GET)
+	public ModelAndView addBanner(ModelAndView modelAndView){
 
-		Banner banner = bannerService.queryBanner(bannerId);
-
-		model.addAttribute("banner", banner);
-
-		return "";
+		modelAndView.setViewName("");
+		return modelAndView;
 	}
 
-	// 3.修改banner提交
-	public ModelAndView updateBanner(ModelAndView modelAndView, Banner banner) {
+	@RequestMapping(value="/addBannerSubmit",method = RequestMethod.POST)
+    public ModelAndView addBannerSubmit(ModelAndView modelAndView,Banner banner){
+
+		if(StringUtils.isEmpty(banner.getType())){
+
+		}
+		bannerService.createBanner(banner);
+		return modelAndView;
+	}
+
+
+    @RequestMapping(value="/updateBanner",method = RequestMethod.GET)
+	public ModelAndView updateBanner(Integer bannerId,ModelAndView modelAndView) {
+
+		Banner banner = bannerService.queryBanner(bannerId);
+		modelAndView.addObject("banner",banner);
+		return modelAndView;
+	}
+
+	@RequestMapping(value="/updateBannerSubmit",method = RequestMethod.POST)
+	public ModelAndView updateBannerSubmit(ModelAndView modelAndView, Banner banner) {
 
 		bannerService.updateBanner(banner);
 
 		return modelAndView;
 	}
 
-	// 4.根据id删除一个banner
-	public String deleteBannerById(Integer bannerId) {
+	@RequestMapping(value="/deleteBannerById",method= RequestMethod.GET)
+	public ModelAndView deleteBannerById(Integer bannerId,ModelAndView modelAndView) {
 
 		bannerService.deleteBanner(bannerId);
 
-		return "";
+		return modelAndView;
 	}
 
 	public BannerService getBannerService() {
