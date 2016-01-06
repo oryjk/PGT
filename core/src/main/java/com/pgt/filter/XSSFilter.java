b681badf95e1c75488fb303184a3f108e9397db7
Carl Wang <oryjk@qq.com>
2016-01-07 01:06:02 +0800

    add

--- core/src/main/java/com/pgt/filter/XSSFilter.java
+++ core/src/main/java/com/pgt/filter/XSSFilter.java
@@ -18,6 +18,7 @@ public class XSSFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         chain.doFilter(new XSSRequestWrapper((HttpServletRequest) request), response);
+        return;
     }
 
     @Override
