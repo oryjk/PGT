48f0f21e1d50a44ad5b735e7583fb74a45a70572
Carl Wang <oryjk@qq.com>
2016-01-06 13:22:33 +0800

    add product index

--- core/src/main/java/com/pgt/media/MediaServiceImp.java
+++ core/src/main/java/com/pgt/media/MediaServiceImp.java
@@ -1,12 +1,16 @@
 package com.pgt.media;
 
+import com.pgt.base.service.TransactionService;
 import com.pgt.common.bean.Media;
 import com.pgt.common.dao.MediaMapper;
 import com.pgt.media.bean.MediaType;
 import com.pgt.product.bean.ProductMedia;
 import com.pgt.product.dao.ProductMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionStatus;
 
 import java.util.List;
 
@@ -14,7 +18,8 @@ import java.util.List;
  * Created by carlwang on 12/7/15.
  */
 @Service
-public class MediaServiceImp implements MediaService {
+public class MediaServiceImp extends TransactionService implements MediaService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MediaServiceImp.class);
     @Autowired
     private ProductMapper productMapper;
     @Autowired
@@ -63,7 +68,15 @@ public class MediaServiceImp implements MediaService {
 
     @Override
     public Integer create(ProductMedia productMedia) {
-        mediaMapper.createMedia(productMedia);
+        TransactionStatus transactionStatus = ensureTransaction();
+        try {
+            mediaMapper.createMedia(productMedia);
+        } catch (Exception e) {
+            LOGGER.error("Can not create product media.");
+            getTransactionManager().rollback(transactionStatus);
+        } finally {
+            getTransactionManager().commit(transactionStatus);
+        }
         return productMedia.getId();
     }
 
