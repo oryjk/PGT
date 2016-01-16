2aac02029e851bbe2a0dd2a8d34781ee76a2423c
liqiang <2484232069@qq.com>
2016-01-16 20:17:08 +0800

    web filter alter

--- core/src/main/java/com/pgt/constant/PathConstant.java
+++ core/src/main/java/com/pgt/constant/PathConstant.java
@@ -7,6 +7,6 @@ public class PathConstant {
 
     public static String LOGIN_PATH = "/WEB-INF/views/jsp/user/login.jsp";
     public static String RESET_PASSWORD = ".*/((resetPassword))";
-    public static String NO_LOGIN_INTERCEPTOR_PATH = ".*/((login))|.*/((logout))|.*/((updatePassword))|.*/((updatePasswordSubmit))|.*/((register))|.*/((mLogin))|.*/((mRegister))|.*/((resetPassword))|.*/((yeepayAccountInfo))|.*/web/.*|.*resources/static/.*";
+    public static String NO_LOGIN_INTERCEPTOR_PATH = ".*/((login))|.*/((logout))|.*/((updatePassword))|.*/((updatePasswordSubmit))|.*/((register))|.*/((mLogin))|.*/((mRegister))|.*/((resetPassword))|.*/((yeepayAccountInfo))|.*/web/.*|.*resources/.*";
     public static String NO_LOGIN_TOKEN_PATH = ".*/((login))|.*/((logout))|.*/((updatePassword))|.*/((updatePasswordSubmit))|.*/((register))|.*/((mLogin))|.*/((mRegister))|.*/((resetPassword))|.*/((yeepayAccountInfo))|.((mHome))|.((mEssearch))/.*|.((mProduct))/.*|.((mConsulting))/((query))|.((mDiscuss))/((query))";
 }
