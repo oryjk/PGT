4a905c355f81dc0723d0dcd2865ebe97bb0cd86d
Carl Wang <oryjk@qq.com>
2016-01-19 16:17:48 +0800

    add shpping fee in order,change orderType to type in orderMapper

--- core/src/main/java/com/pgt/tender/service/TenderService.java
+++ core/src/main/java/com/pgt/tender/service/TenderService.java
@@ -5,6 +5,7 @@ import com.pgt.tender.mapper.TenderMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
