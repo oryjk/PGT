2c5aa8db1d185b262b7a8799865a0350a9ec2899
liqiang <2484232069@qq.com>
2016-01-19 14:03:26 +0800

    web search add

---
+++ mobile/src/test/java/SearchTest.java
@@ -0,0 +1,45 @@
+import com.alibaba.fastjson.JSONObject;
+import com.pgt.common.bean.CommPaginationBean;
+import com.pgt.mobile.search.controller.ESSearchMobileController;
+import com.pgt.mobile.search.controller.EssearchBean;
+import com.pgt.search.bean.ESAggregation;
+import com.pgt.search.bean.ESRange;
+import com.pgt.search.bean.ESSort;
+import com.pgt.search.bean.ESTerm;
+import com.pgt.search.service.ESSearchService;
+import com.pgt.utils.PaginationBean;
+import com.pgt.web.search.AbstractController.SearchBaseController;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by liqiang on 16-1-18.
+ */
+//@Ignore
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:spring-core-config.xml")
+public class SearchTest extends SearchBaseController{
+    private static final Logger LOGGER = LoggerFactory.getLogger(SearchTest.class);
+
+    @Test
+    public void testeSearch() {
+        CommPaginationBean paginationBean = new CommPaginationBean(20, 1, 100);
+        paginationBean.setCurrentIndex(0);
+        EssearchBean essearchBean = new EssearchBean();
+        essearchBean.setTerm("玉");
+        LOGGER.debug("******************" + JSONObject.toJSONString(this.essearchService(essearchBean)));
+        LOGGER.debug("******************" + JSONObject.toJSONString(this.findCategories()));
+    }
+}
