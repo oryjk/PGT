7eec8e84c92f3804666413d2cd2de328a7c85125
周家梁 <zhoujialiang@zhoujialiangdeMacBook-Air.local>
2016-01-28 18:30:40 +0800

    fax

--- b2c_store/src/main/webapp/resources/juedangpin/shopping-cart/shopping-cart.js
+++ b2c_store/src/main/webapp/resources/juedangpin/shopping-cart/shopping-cart.js
@@ -20,63 +20,6 @@ require(['jquery', 'component', 'product'], function($, Cpn, Prd) {
             tabTarget: $('#tab h2')
         });
 
-        //弹出框
-        Cpn.pop({
-            popUp: $('#popUp'),
-            close: $('#popClose, #popReset')
-        });
-        $('.link-btn').click(function() {
-            $('#popUp').fadeIn(300);
-        });
-
-        var areaObj = {
-            province: $('#province'),
-            city: $('#city'),
-            country: $('#country')
-        };
-
-        $('#addAddress').click(function() {
-            $('#popUp').fadeIn(300);
-        });
-
-
-        //省->市
-        Cpn.select2(areaObj.province, function(id) {
-            var url = Prd.baseUrl + '/getCityByProvinceId/' + id;
-            $.ajax({
-                url: url,
-                data: null,
-                success: function(param) {
-                    var str = '';
-                    $.each($.parseJSON(param), function() {
-                        str += '<li><a class="option-view" data-value="'+ this.id+'" href="#">'+ this.name+'</a></li>'
-                    });
-                    $('#city').children('.selected').html('请选择').end().siblings('.options').html(str).next('.select-value').val('');
-                    $('#country').children('.selected').html('请选择').end().siblings('.options').html('').next('.select-value').val('');
-                }
-            })
-        });
-
-        //市->区
-        Cpn.select2(areaObj.city, function(id) {
-            var url = Prd.baseUrl + '/getAreaByCityId/' + id;
-            $.ajax({
-                type: 'get',
-                url: url,
-                data: null,
-                success: function(param) {
-                    var str = '';
-                    $.each($.parseJSON(param), function() {
-                        str += '<li><a class="option-view" data-value="'+ this.id+'" href="#">'+ this.name+'</a></li>'
-                    });
-                    $('#country').children('.selected').html('请选择').end().siblings('.options').html(str).next('.select-value').val('');
-                }
-            })
-        });
-
-        //区
-        Cpn.select2(areaObj.country);
-
         $('#popSubmit').click(function(e){
             e.preventDefault();
             var $this = $(this);
