936debeef599562c7908485de7588da3cc0cffd7
jeniss <jenisstian@hotmail.com>
2016-02-06 00:09:30 +0800

    2016-02-06 00:07 Refactoring interceptor for recommend, browsed & favourite

--- b2c_store/src/main/java/com/pgt/cart/filter/RecommendInterceptor.java
+++ b2c_store/src/main/java/com/pgt/cart/filter/RecommendInterceptor.java
@@ -1,24 +1,14 @@
 package com.pgt.cart.filter;
 
 import com.pgt.cart.constant.CartConstant;
+import com.pgt.cart.constant.SessionConstant;
 import com.pgt.cart.service.ProductBrowseTrackService;
 import com.pgt.cart.service.RecommendService;
-import com.pgt.cart.service.UserFavouriteService;
-import com.pgt.constant.UserConstant;
-import com.pgt.cart.bean.pagination.InternalPagination;
-import com.pgt.cart.bean.pagination.InternalPaginationBuilder;
-import com.pgt.cart.constant.SessionConstant;
 import com.pgt.cart.util.RepositoryUtils;
 import com.pgt.product.bean.Product;
-import com.pgt.user.bean.User;
-import com.pgt.user.service.RecentlyViewService;
 import org.apache.commons.collections.CollectionUtils;
-import org.elasticsearch.search.SearchHit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -30,7 +20,7 @@ import java.util.List;
 /**
  * Created by Yove on 12/3/2015.
  */
-public class RecommendInterceptor implements HandlerInterceptor {
+public class RecommendInterceptor extends ItemLoadInterceptor {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(RecommendInterceptor.class);
 
@@ -40,30 +30,20 @@ public class RecommendInterceptor implements HandlerInterceptor {
 	@Resource(name = "productBrowseTrackService")
 	private ProductBrowseTrackService mBrowseTrackService;
 
-	@Autowired
-	private RecentlyViewService mRecentlyViewService;
-
-	@Resource(name = "userFavouriteService")
-	private UserFavouriteService mUserFavouriteService;
-
-
 	@Resource(name = "productBrowseTrackService")
 	private ProductBrowseTrackService mProductBrowseTrackService;
 
 
 	@Override
-	public boolean preHandle(final HttpServletRequest pRequest, final HttpServletResponse pResponse, final Object pHandler) throws Exception {
-		if (!getRecommendService().recommend(pRequest)) {
-			return true;
-		}
+	public boolean loadItems(final HttpServletRequest pRequest, final HttpServletResponse pResponse, final Object pHandler) {
 		List<Integer> productIds = (List<Integer>) pRequest.getSession().getAttribute(SessionConstant.RECENT_PRODUCT_IDS);
-		if(CollectionUtils.isEmpty(productIds)){
-			productIds= new ArrayList<>();
+		if (CollectionUtils.isEmpty(productIds)) {
+			productIds = new ArrayList<>();
 			LinkedList<String> browsedProductIds = getProductBrowseTrackService().getBrowsedProductIdsFromCookies(pRequest);
-			for (String id:browsedProductIds) {
-                productIds.add(Integer.parseInt(id));
+			for (String id : browsedProductIds) {
+				productIds.add(Integer.parseInt(id));
 			}
-			pRequest.getSession().setAttribute(SessionConstant.RECENT_PRODUCT_IDS,productIds);
+			pRequest.getSession().setAttribute(SessionConstant.RECENT_PRODUCT_IDS, productIds);
 		}
 		LOGGER.debug("Get recently browsed products: " + productIds);
 		String idString = getBrowseTrackService().getRequestedProductId(pRequest);
@@ -72,31 +52,9 @@ public class RecommendInterceptor implements HandlerInterceptor {
 		List<Product> recommendProducts = getRecommendService().figureRecommendProducts(productIds, currentProductId);
 		LOGGER.debug("Get recommend products: " + recommendProducts);
 		pRequest.setAttribute(CartConstant.RECOMMEND_PRODUCTS, recommendProducts);
-		// append logic to load favourites & recently browsed products
-		List<SearchHit> recentBrowsedProducts = getRecentlyViewService().getRecentBrowsedProducts(pRequest.getSession());
-		pRequest.setAttribute(CartConstant.BROWSED_PRODUCTS, recentBrowsedProducts);
-		User currentUser = getCurrentUser(pRequest);
-		if (currentUser != null) {
-			InternalPagination favouritePage = InternalPaginationBuilder.createDefaultInternalPagination();
-			getUserFavouriteService().queryFavouritePage(currentUser.getId().intValue(), favouritePage);
-			pRequest.setAttribute(CartConstant.FAVOURITES, favouritePage);
-		}
 		return true;
 	}
 
-	@Override
-	public void postHandle(final HttpServletRequest pRequest, final HttpServletResponse pResponse, final Object pHandler, final ModelAndView modelAndView) throws Exception {
-
-	}
-
-	@Override
-	public void afterCompletion(final HttpServletRequest pRequest, final HttpServletResponse pResponse, final Object pHandler, final Exception ex) throws Exception {
-
-	}
-
-	protected User getCurrentUser(HttpServletRequest pRequest) {
-		return (User) pRequest.getSession().getAttribute(UserConstant.CURRENT_USER);
-	}
 
 	public RecommendService getRecommendService() {
 		return mRecommendService;
@@ -112,22 +70,6 @@ public class RecommendInterceptor implements HandlerInterceptor {
 
 	public void setBrowseTrackService(final ProductBrowseTrackService pBrowseTrackService) {
 		mBrowseTrackService = pBrowseTrackService;
-	}
-
-	public RecentlyViewService getRecentlyViewService() {
-		return mRecentlyViewService;
-	}
-
-	public void setRecentlyViewService(final RecentlyViewService pRecentlyViewService) {
-		mRecentlyViewService = pRecentlyViewService;
-	}
-
-	public UserFavouriteService getUserFavouriteService() {
-		return mUserFavouriteService;
-	}
-
-	public void setUserFavouriteService(final UserFavouriteService pUserFavouriteService) {
-		mUserFavouriteService = pUserFavouriteService;
 	}
 
 	public ProductBrowseTrackService getProductBrowseTrackService() {
