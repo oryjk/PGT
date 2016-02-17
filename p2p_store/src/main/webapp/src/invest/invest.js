306a1b472e1ccb9697033a8be53578bf9f719955
supersoup <819127251@qq.com>
2016-02-17 09:10:15 +0800

    invest page is ok

--- p2p_store/src/main/webapp/src/invest/invest.js
+++ p2p_store/src/main/webapp/src/invest/invest.js
@@ -12,47 +12,52 @@ require.config({
 require(['jquery', 'component', 'ajax'], function($, Cpn, Ajax) {
     $(document).ready(function() {
 
-        var areaObj = {
-            province: $('#province'),
-            city: $('#city'),
-            country: $('#country')
-        };
+        //为仿select绑定事件
+        var selectBox = $('.invest-gender-select,.invest-type-select');
+        Cpn.select(selectBox.find('.select-view'), selectBox.find('.option-view'));
 
-        //省->市
-        Cpn.select2(areaObj.province, function (id) {
-            var url = Ajax.baseUrl + '/getCityByProvinceId/' + id;
-            $.ajax({
-                url: url,
-                data: null,
-                success: function (param) {
-                    var str = '';
-                    $.each($.parseJSON(param), function () {
-                        str += '<li><a class="option-view" data-value="' + this.id + '" href="#">' + this.name + '</a></li>'
-                    });
-                    $('#city').children('.selected').html('请选择').end().siblings('.options').html(str).next('.select-value').val('');
-                    $('#country').children('.selected').html('请选择').end().siblings('.options').html('').next('.select-value').val('');
-                }
-            })
-        });
+        //定义省市县三级联动.
+        (function () {
+            var areaObj = {
+                province: $('#province'),
+                city: $('#city'),
+                country: $('#country')
+            };
+            //选省获市
+            Cpn.select2(areaObj.province, function (id) {
+                var url = Ajax.baseUrl + '/getCityByProvinceId/' + id;
+                $.ajax({
+                    url: url,
+                    data: null,
+                    success: function (param) {
+                        var str = '';
+                        $.each($.parseJSON(param), function () {
+                            str += '<li><a class="option-view" data-value="' + this.id + '" href="#">' + this.name + '</a></li>'
+                        });
+                        $('#city').children('.selected').html('请选择').end().siblings('.options').html(str).next('.select-value').val('');
+                        $('#country').children('.selected').html('请选择').end().siblings('.options').html('').next('.select-value').val('');
+                    }
+                })
+            });
+            //选市获县
+            Cpn.select2(areaObj.city, function (id) {
+                var url = Ajax.baseUrl + '/getAreaByCityId/' + id;
+                $.ajax({
+                    type: 'get',
+                    url: url,
+                    data: null,
+                    success: function (param) {
+                        var str = '';
+                        $.each($.parseJSON(param), function () {
+                            str += '<li><a class="option-view" data-value="' + this.id + '" href="#">' + this.name + '</a></li>'
+                        });
+                        $('#country').children('.selected').html('请选择').end().siblings('.options').html(str).next('.select-value').val('');
+                    }
+                })
+            });
+            //县的下拉
+            Cpn.select2(areaObj.country);
+        }());
 
-        //市->区
-        Cpn.select2(areaObj.city, function (id) {
-            var url = Ajax.baseUrl + '/getAreaByCityId/' + id;
-            $.ajax({
-                type: 'get',
-                url: url,
-                data: null,
-                success: function (param) {
-                    var str = '';
-                    $.each($.parseJSON(param), function () {
-                        str += '<li><a class="option-view" data-value="' + this.id + '" href="#">' + this.name + '</a></li>'
-                    });
-                    $('#country').children('.selected').html('请选择').end().siblings('.options').html(str).next('.select-value').val('');
-                }
-            })
-        });
-
-        //区
-        Cpn.select2(areaObj.country);
     });
 });
