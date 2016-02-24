a5ac822f91bfbcd87e627553018dfe3bed263a1a
zhangxiaodong <625188964@qq.com>
2016-02-24 22:57:44 +0800

    Merge branch 'develop' of https://git.coding.net/carlwang/PGT into develop
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

---
+++ core/src/main/java/com/pgt/report/categroy_sale_statistics/dao/SaleStatisticsMapper.java
@@ -0,0 +1,18 @@
+package com.pgt.report.categroy_sale_statistics.dao;
+
+import com.pgt.base.mapper.SqlMapper;
+import com.pgt.report.categroy_sale_statistics.bean.SaleStatisticsBean;
+import com.pgt.report.categroy_sale_statistics.bean.RootCategroyBean;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * Created by liqiang on 02/24/16.
+ */
+@Component
+public interface SaleStatisticsMapper extends SqlMapper {
+    List<SaleStatisticsBean> reportSubCategroyStocks();
+    RootCategroyBean reportSelectRootCategroy(int i);
+}
