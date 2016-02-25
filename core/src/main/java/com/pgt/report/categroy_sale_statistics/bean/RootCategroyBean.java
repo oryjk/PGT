f6dbdd81a5b9bd183a54c4254d0bee0c7bbd86f1
liqiang <2484232069@qq.com>
2016-02-25 18:27:57 +0800

    report test version1

--- core/src/main/java/com/pgt/report/categroy_sale_statistics/bean/RootCategroyBean.java
+++ core/src/main/java/com/pgt/report/categroy_sale_statistics/bean/RootCategroyBean.java
@@ -4,8 +4,13 @@ package com.pgt.report.categroy_sale_statistics.bean;
  * Created by liqiang on 16-2-24.
  */
 public class RootCategroyBean {
+    //父分类id
+    private Integer cate_id;
+    //父分类id
     private Integer id;
     private String name;
+    private Integer sale_stocks;
+    private Integer sale_prices;
 
     public String getName() {
         return name;
@@ -13,6 +18,30 @@ public class RootCategroyBean {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Integer getSale_stocks() {
+        return sale_stocks;
+    }
+
+    public void setSale_stocks(Integer sale_stocks) {
+        this.sale_stocks = sale_stocks;
+    }
+
+    public Integer getSale_prices() {
+        return sale_prices;
+    }
+
+    public void setSale_prices(Integer sale_prices) {
+        this.sale_prices = sale_prices;
+    }
+
+    public Integer getCate_id() {
+        return cate_id;
+    }
+
+    public void setCate_id(Integer cate_id) {
+        this.cate_id = cate_id;
     }
 
     public Integer getId() {
---
+++ core/src/main/java/com/pgt/report/categroy_sale_statistics/bean/SaleCategroyBean.java
@@ -0,0 +1,26 @@
+package com.pgt.report.categroy_sale_statistics.bean;
+
+/**
+ * Created by liqiang on 16-2-24.
+ */
+public class SaleCategroyBean {
+    private Integer saleStocks = 0;
+    private Integer salePrices = 0;
+
+    public Integer getSaleStocks() {
+        return saleStocks;
+    }
+
+    public void setSaleStocks(Integer saleStocks) {
+        this.saleStocks = saleStocks;
+    }
+
+    public Integer getSalePrices() {
+        return salePrices;
+    }
+
+    public void setSalePrices(Integer salePrices) {
+        this.salePrices = salePrices;
+    }
+
+}
