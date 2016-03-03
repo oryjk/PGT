cddb6d6fe6ccceff12a396a63511f175801dff6f
Carl Wang <oryjk@qq.com>
2016-03-04 01:04:13 +0800

    add pagination

--- p2p_store/src/main/webapp/resources/product-list/productList.js
+++ p2p_store/src/main/webapp/resources/product-list/productList.js
@@ -10,7 +10,7 @@ require.config({
         handlebar: '../core/js/handlebars-v4.0.5',
         ajax: '../core/js/module/ajax',
         underscore: '../core/js/underscore',
-        vue: '../core/js/vue.min',
+        vue: '../core/js/vue',
         component: '../core/js/module/component'
     }
 });
@@ -24,8 +24,7 @@ require(['jquery', 'handlebar', 'ajax', 'underscore', 'vue', 'component'], funct
             }
             return false;
         });
-        Handlebars.registerHelper('pagination', function (currentPage, exceptPage)
-        {
+        Handlebars.registerHelper('pagination', function (currentPage, exceptPage) {
             if (currentPage < exceptPage) {
                 return true;
             }
@@ -44,81 +43,120 @@ require(['jquery', 'handlebar', 'ajax', 'underscore', 'vue', 'component'], funct
         var vm = new Vue({
             el: '#mainContent',
             data: {
-                sort: Con.getParamValue(window.location.href, 'sort') != null ? Con.getParamValue(window.location.href, 'sort') : '',
-                page: Con.getParamValue(window.location.href, 'page') != null ? Con.getParamValue(window.location.href, 'page') : '',
-                keyword: Con.getParamValue(window.location.href, 'keyword') != null ? Con.getParamValue(window.location.href, 'keyword') : '',
-                tenderFilter: Con.getParamValue(window.location.href, 'tenderFilter') != null ? Con.getParamValue(window.location.href, 'tenderFilter') : '',
-                cid: Con.getParamValue(window.location.href, 'cid') != null ? Con.getParamValue(window.location.href, 'cid') : ''
+                queryRequest: {
+                    sort: Con.getParamValue(window.location.href, 'sort') != null ? Con.getParamValue(window.location.href, 'sort') : '',
+                    page: Con.getParamValue(window.location.href, 'page') != null ? Con.getParamValue(window.location.href, 'page') : '',
+                    keyword: Con.getParamValue(window.location.href, 'keyword') != null ? Con.getParamValue(window.location.href, 'keyword') : '',
+                    tenderFilter: Con.getParamValue(window.location.href, 'tenderFilter') != null ? Con.getParamValue(window.location.href, 'tenderFilter') : '',
+                    cid: Con.getParamValue(window.location.href, 'cid') != null ? Con.getParamValue(window.location.href, 'cid') : '',
+                },
+                ajax: false,
+                isPre: false,
+                isPost: false,
+                isOther: false,
+                pageList: '',
+                pagination: ''
             },
             methods: {
                 sortBy: function (event) {
                     event.preventDefault();
                     this.sort = $(event.target).attr('data-value') ? $(event.target).attr('data-value') : '';
-                    sendRequest(vm);
+                    vm.sendRequest(vm);
                 },
                 tenderFilterAction: function (event) {
                     this.tenderFilter = $(event.target).attr('data-value') ? $(event.target).attr('data-value') : '';
-                    sendRequest(vm);
+                    vm.sendRequest(vm);
                 },
                 paginationAction: function (event) {
-                    this.page = $(event.target).attr('data-value') ? $(event.target).attr('data-value') : '';
-                    $(event.target).addClass('current-page');
-                    sendRequest(vm);
+                    if ($(event.target).attr('class') == 'pre') {
+                        this.queryRequest.page = this.queryRequest.page == '' ? 1 : this.queryRequest.page - 1;
+                        this.sendRequest(vm);
+                        return;
+                    }
+                    if ($(event.target).attr('class') == 'next') {
+                        this.queryRequest.page = this.queryRequest.page == '' ? 2 : this.queryRequest.page + 1;
+                        this.sendRequest(vm);
+                        return;
+                    }
+                    this.queryRequest.page = $(event.target).attr('data-value') ? $(event.target).attr('data-value') : '';
+                    this.sendRequest(vm);
+
                 },
-                onmouseover: function () {
+                sendRequest: function () {
+                    this.ajax = true;
+                    var urlParam = Con.buildURLParamByJson(JSON.parse(JSON.stringify(vm.$data.queryRequest)));
+                    window.history.pushState(null, null, '?' + urlParam);
+                    $.ajax({
+                        url: '/tender/ajaxTenderList',
+                        type: 'GET',
+                        dataType: 'json',
+                        data: JSON.parse(JSON.stringify(vm.$data.queryRequest)),
+                    })
+                        .done(function (response) {
+                            vm.loadTenderList(response)
+                        })
+                        .fail(function () {
+                            console.log("error");
+                        })
+                        .always(function () {
+                            console.log("complete");
+                        });
                 },
+                loadTenderList: function (data) {
+                    console.log(data);
+                    if (data != null) {
+                        this.renderTenders(data);
+                        this.renderPagination(data);
+                    }
+                },
+                renderTenders: function (data) {
+                    $.ajax({
+                        url: '/resources/product-list/tenders_template.html',
+                        type: 'GET',
+                        dataType: 'html',
+                    })
+                        .done(function (response) {
+                            console.log("success");
+                            console.log(response);
+
+                            var template = Handlebars.compile(response);
+                            var html = template(data.data);
+                            $('.products').html(html);
+                        })
+                        .fail(function () {
+                            console.log("error");
+                        })
+                        .always(function () {
+                            console.log("complete");
+                        });
+                },
+                renderPagination: function (data) {
+                    var pagination = data.data.paginationBean;
+                    this.pagination = pagination;
+                    if (pagination.currentPage < 6) {
+                        this.isPre = true;
+                        this.isPost = false;
+                        this.isOther = false;
+                        if (pagination.totalPage > 7) {
+                            this.pageList = 7;
+                            return;
+                        }
+                        this.pageList = pagination.totalPage;
+                    }
+                    if (pagination.totalPage - pagination.currentPage < 3) {
+                        this.isPost = true;
+                        this.isPre = false;
+                        this.isOther = false;
+                        this.pageList = pagination.totalPage;
+                        return;
+                    }
+                    this.isOther = true;
+                    this.pageList = pagination.totalPage;
+                    console.log(pagination);
+                }
+
             }
         });
-
-        function sendRequest(vm) {
-
-            var urlParam = Con.buildURLParamByJson(JSON.parse(JSON.stringify(vm.$data)));
-            window.history.pushState(null, null, '?' + urlParam);
-            $.ajax({
-                url: '/tender/ajaxTenderList',
-                type: 'GET',
-                dataType: 'json',
-                data: JSON.parse(JSON.stringify(vm.$data)),
-            })
-                .done(function (response) {
-                    loadTenderList(response)
-                })
-                .fail(function () {
-                    console.log("error");
-                })
-                .always(function () {
-                    console.log("complete");
-                });
-
-        };
-
-        function loadTenderList(data) {
-            console.log(data);
-            if (data != null) {
-                $.ajax({
-                    url: '/resources/product-list/tenders_template.html',
-                    type: 'GET',
-                    dataType: 'html',
-                })
-                    .done(function (response) {
-                        console.log("success");
-                        console.log(response);
-
-                        var template = Handlebars.compile(response);
-                        var html = template(data.data);
-                        $('.products').html(html);
-                    })
-                    .fail(function () {
-                        console.log("error");
-                    })
-                    .always(function () {
-                        console.log("complete");
-                    });
-
-            }
-
-        }
-
 
 
         console.log(vm);
