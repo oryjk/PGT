5c2c7f0e4806232e4a97cbc5dd04d6e9a684064f
Carl Wang <oryjk@qq.com>
2016-03-03 17:21:46 +0800

    add tender list filter

--- core/src/main/java/com/pgt/constant/ESConstants.java
+++ core/src/main/java/com/pgt/constant/ESConstants.java
@@ -4,8 +4,8 @@ package com.pgt.constant;
  * Created by carlwang on 12/6/15.
  */
 public class ESConstants {
-    public static final String PARENT_CATEGORY_ID = "tender.parentCategoryId";
-    public static final String ROOT_CATEGORY_ID = "tender.rootCategoryId";
+    public static final String PARENT_CATEGORY_ID = "parentCategoryId";
+    public static final String ROOT_CATEGORY_ID = "rootCategoryId";
     public static final String PUBLISH_DATE = "tender.publishDate";
     public static final String DUE_DATE = "tender.dueDate";
     public static final String TENDER_LIST_RESULT = "tenderListResult";
