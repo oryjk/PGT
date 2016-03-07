a67e20e7ea6515db6b0020082f3aa3e00c15a399
zhangxiaodong <625188964@qq.com>
2016-03-07 21:06:11 +0800

    add ThirdLogin table and mapper

---
+++ core/src/main/java/com/pgt/user/service/ThirdLoginService.java
@@ -0,0 +1,19 @@
+package com.pgt.user.service;
+
+import com.pgt.user.bean.ThirdLogin;
+
+/**
+ * Created by zhangxiaodong on 16-3-7.
+ */
+public interface ThirdLoginService {
+
+    void createThirdLogin(ThirdLogin thirdLogin);
+
+    void updateThirdLogin(ThirdLogin thirdLogin);
+
+    void deleteThirdLoginById(Integer id);
+
+    ThirdLogin queryThirdLoginById(Integer id);
+
+
+}
