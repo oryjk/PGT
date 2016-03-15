e4b91f3221b6aa712a74af4662182ae4165b1a95
supersoup <819127251@qq.com>
2016-03-15 22:10:14 +0800

    buy address

---
+++ p2p_store/src/main/webapp/resources/buy/buy.js
@@ -0,0 +1,24 @@
+/**
+ * Created by supersoup on 16/3/15.
+ */
+require.config({
+    paths: {
+        jquery: '../core/js/jquery.min',
+        component: '../core/js/module/component',
+        ajax: '../core/js/module/ajax'
+    }
+});
+
+require(['jquery', 'component', 'ajax'], function ($, Cpn, Ajax) {
+    var pop = $('#popUp');
+    Cpn.pop({
+        popUp: pop,
+        close: $('#popReset, #popClose')
+    });
+    $('#addNewAddress').click(function () {
+        pop.fadeIn(300);
+    });
+    $(document).on('click', '.receive-modify', function () {
+        pop.fadeIn(300);
+    })
+});
