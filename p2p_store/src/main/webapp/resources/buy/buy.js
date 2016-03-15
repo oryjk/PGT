2fd812ab5b4e771a50726aa80c3ac277eb4ab7d0
supersoup <819127251@qq.com>
2016-03-15 23:22:11 +0800

    select view

--- p2p_store/src/main/webapp/resources/buy/buy.js
+++ p2p_store/src/main/webapp/resources/buy/buy.js
@@ -20,5 +20,48 @@ require(['jquery', 'component', 'ajax'], function ($, Cpn, Ajax) {
     });
     $(document).on('click', '.receive-modify', function () {
         pop.fadeIn(300);
-    })
+    });
+
+
+    var areaObj = {
+        province: $('#province'),
+        city: $('#city'),
+        country: $('#country')
+    };
+    //省->市
+    Cpn.select2(areaObj.province, function(id) {
+        var url = Prd.baseUrl + '/getCityByProvinceId/' + id;
+        $.ajax({
+            url: url,
+            data: null,
+            success: function(param) {
+                var str = '';
+                $.each($.parseJSON(param), function() {
+                    str += '<li><a class="option-view" data-value="'+ this.id+'" href="#">'+ this.name+'</a></li>'
+                });
+                $('#city').children('.selected').html('请选择').end().siblings('.options').html(str).next('.select-value').val('');
+                $('#country').children('.selected').html('请选择').end().siblings('.options').html('').next('.select-value').val('');
+            }
+        })
+    });
+
+    //市->区
+    Cpn.select2(areaObj.city, function(id) {
+        var url = Prd.baseUrl + '/getAreaByCityId/' + id;
+        $.ajax({
+            type: 'get',
+            url: url,
+            data: null,
+            success: function(param) {
+                var str = '';
+                $.each($.parseJSON(param), function() {
+                    str += '<li><a class="option-view" data-value="'+ this.id+'" href="#">'+ this.name+'</a></li>'
+                });
+                $('#country').children('.selected').html('请选择').end().siblings('.options').html(str).next('.select-value').val('');
+            }
+        })
+    });
+
+    //区
+    Cpn.select2(areaObj.country);
 });
