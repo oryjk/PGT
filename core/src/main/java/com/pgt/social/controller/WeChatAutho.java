78614dfbb26c5bbb535993fa7491979c33a94539
Carl Wang <oryjk@qq.com>
2016-03-29 18:23:48 +0800

    fix chat

--- core/src/main/java/com/pgt/social/controller/WeChatAutho.java
+++ core/src/main/java/com/pgt/social/controller/WeChatAutho.java
@@ -1,29 +1,28 @@
 package com.pgt.social.controller;
 
-import java.util.Random;
-
 /**
  * Created by zhangxiaodong on 16-3-18.
  */
 public class WeChatAutho {
 
-        /// <summary>
-        /// 获取微信Code
-        /// </summary>
-        /// <param name="appId"></param>
-        /// <param name="appSecret"></param>
-        /// <param name="redirectUrl"></param>
-        public String GetWeiXinCode(String appId,String appSecret,String redirectUrl)
-        {
-            //微信登录授权
-            //string url = "https://open.weixin.qq.com/connect/qrconnect?appid=" + appId + "&redirect_uri=" + redirectUrl +"&response_type=code&scope=snsapi_login&state=STATE#wechat_redirect";
-            //微信OpenId授权
-            //string url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + appId + "&redirect_uri=" + redirectUrl +"&response_type=code&scope=snsapi_login&state=STATE#wechat_redirect";
-            //微信用户信息授权
-            String url = "https://open.weixin.qq.com/connect/qrconnect?appid=" + appId + "&redirect_uri=" + redirectUrl + "&response_type=code&scope=snsapi_login&state="+appSecret+"#wechat_redirect";
-            return url;
-        }
-
+    /// <summary>
+    /// 获取微信Code
+    /// </summary>
+    /// <param name="appId"></param>
+    /// <param name="appSecret"></param>
+    /// <param name="redirectUrl"></param>
+    public String GetWeiXinCode(String appId, String appSecret, String redirectUrl) {
+        //微信登录授权
+        //string url = "https://open.weixin.qq.com/connect/qrconnect?appid=" + appId + "&redirect_uri=" + redirectUrl
+        // +"&response_type=code&scope=snsapi_login&state=STATE#wechat_redirect";
+        //微信OpenId授权
+        //string url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + appId + "&redirect_uri=" + redirectUrl
+        // +"&response_type=code&scope=snsapi_login&state=STATE#wechat_redirect";
+        //微信用户信息授权
+        String url = "https://open.weixin.qq.com/connect/qrconnect?appid=" + appId + "&redirect_uri=" + redirectUrl +
+                "&response_type=code&scope=snsapi_login#wechat_redirect";
+        return url;
+    }
 
 
 }
--- core/src/main/java/com/pgt/social/controller/WeChatController.java
+++ core/src/main/java/com/pgt/social/controller/WeChatController.java
@@ -15,7 +15,6 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.net.URLEncoder;
 
 /**
  * Created by zhangxiaodong on 16-3-18.
@@ -35,22 +34,19 @@ public class WeChatController {
     @Autowired
     private UserInformationService userInformationService;
 
-    private String appId="wxc0ba761c7a33d4d5";
+    private String appId = "wxc0ba761c7a33d4d5";
 
-    private String redirect_uri="dev.p2p.dianjinzi.com%2fweChatLogin%2fafterLogin";
-
-    private String requestCode="https://open.weixin.qq.com/connect/qrconnect?appid="+appId+"&response_type=code&scope=snsapi_login";
-
+    private String redirect_uri = "http%3A%2F%2Fdev.p2p.dianjinzi.com%2FweChatLogin%2FafterLogin";
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public void login(HttpServletRequest request, HttpServletResponse response){
+    public void login(HttpServletRequest request, HttpServletResponse response) {
         try {
 
             WeChatAutho weChatAutho = new WeChatAutho();
-            String sessionId=request.getSession().getId();
-            String m=weChatAutho.GetWeiXinCode(appId, DigestUtils.md5Hex(sessionId),redirect_uri);
+            String sessionId = request.getSession().getId();
+            String m = weChatAutho.GetWeiXinCode(appId, DigestUtils.md5Hex(sessionId), redirect_uri);
 
-            LOGGER.debug("The return is:{}",m);
+            LOGGER.debug("The return is:{}", m);
             response.sendRedirect(m);
         } catch (IOException e) {
             e.printStackTrace();
