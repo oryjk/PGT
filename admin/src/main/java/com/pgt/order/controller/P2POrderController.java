9119f79d76c5ddad336919e3d6fcd93a0e8333d0
SamLi <532680211@qq.com>
2016-03-31 23:08:43 +0800

    p2p admin

--- admin/src/main/java/com/pgt/order/controller/P2POrderController.java
+++ admin/src/main/java/com/pgt/order/controller/P2POrderController.java
@@ -2,8 +2,43 @@ package com.pgt.order.controller;
 
 import com.pgt.cart.bean.OrderStatus;
 import com.pgt.internal.controller.InternalTransactionBaseController;
+import com.pgt.order.P2POrderService;
+import com.pgt.order.service.B2COrderService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * Created by Yove on 16/2/7.
  */
-public class P2POrderController extends InternalTransactionBaseController implements OrderStatus {}
+@RequestMapping("/order")
+@RestController
+public class P2POrderController extends InternalTransactionBaseController implements OrderStatus {
+
+
+    @Resource(name = "B2COrderService")
+    private B2COrderService mB2COrderService;
+
+    @Resource(name = "p2pOrderService")
+    private P2POrderService p2pOrderService;
+
+    
+
+
+    public B2COrderService getmB2COrderService() {
+        return mB2COrderService;
+    }
+
+    public void setmB2COrderService(B2COrderService mB2COrderService) {
+        this.mB2COrderService = mB2COrderService;
+    }
+
+    public P2POrderService getP2pOrderService() {
+        return p2pOrderService;
+    }
+
+    public void setP2pOrderService(P2POrderService p2pOrderService) {
+        this.p2pOrderService = p2pOrderService;
+    }
+}
